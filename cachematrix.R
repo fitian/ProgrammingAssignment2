9bfc590c694630e70c24a16617afde93fd815c1b
fitian <69148824+fitian@users.noreply.github.com>
2020-08-09 12:09:00 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
+This assignment is about solving the inverse of a matrix by caching the 
+# result within a lexical scope of a function:  "makeCacheMatrix" and 
+# "cacheSolve". Caching is about using memory to avoid excess computation.
 makeCacheMatrix <- function(x = matrix()) {
-
+   inv<-NULL
+   set<-function(y){
+       x<<-y
+       inv<<-NULL }
+   get<-function() {x}
+   setInverse<-function(inverse){inv<<-inverse}
+   getInverse<-function(){inv}
+   list(set=set,get=get,setInverse=setInverse, getInverse=getInverse)
 }
 
+## The function "cacheSolve" returns the inverse of the matrix that is 
+# returned by makeCacheMatrix function, e.g. xMat$getmatrix()
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+   inv<-x$getInverse()
+   if(!is.null(inver){
+        message("getting catched data")
+        return(inv)}
+   mat<-x$get()
+   inv<-solve(mat,...)
+   x$setInverse(inv)
+   inv
